b3018c8b692b2c211fb0e374371fcf10d0cde500
AutoRacha <autoracha@storacha.network>
2026-02-25 09:39:50 +0000

    fix: use .js extensions and consistent Block import in encoder.ts

--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -1,8 +1,8 @@
 import { createFileEncoderStream } from "@storacha/upload-client/unixfs";
-import { CryptoConfig, Encoder } from "../types";
+import { CryptoConfig, Encoder } from "../types/index.js";
 import { BlobLike } from "@storacha/client/types";
-import { encryptToBlockStream } from "./crypto";
-import { Block } from "multiformats/block/interface";
+import { encryptToBlockStream } from "./crypto.js";
+import type { Block } from "multiformats";
 
 export const makeEncoder = (cryptoConfig: CryptoConfig | null): Encoder => {
   if (cryptoConfig) {
